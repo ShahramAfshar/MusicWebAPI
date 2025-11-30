622c180f9e13baaa0b156de5b31378eff824ee5f
sh.afshar <sh.afshar@dotin.ir>
2025-11-30 09:12:54 +0330

    Add class

---
+++ MusicWebAPI.UnitTests/Post-Implementations/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicWebAPI.UnitTests.Post_Implementations
+{
+    internal class Class1
+    {
+    }
+}
